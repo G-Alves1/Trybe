84185a624d31b9245b4004b795aec7f5b4fe5b4b
G-Alves1 <2kgabrielalves@gmail.com>
2023-01-06 07:02:30 -0300

    feat(): Block 06 - Day 01/02

---
+++ 01_Fundamentos-do-Desenvolvimento-Web/Bloco06_HTML-e-CSS_Forms-FlexBox-e-Responsivo/Dia01-e-Dia02_HTML-e-CSS_Form-e-Bibliotecas-JavaScript-e-Frameworks-CSS/script.js
@@ -0,0 +1,13 @@
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener('click', (e) => e.preventDefault());
+
+INPUT_CHECKBOX.addEventListener('click', (e) => e.preventDefault());
+
+INPUT_TEXT.addEventListener('keypress', (e) => {
+  const character = e.key;
+
+  if (character !== 'a') e.preventDefault();
+});
