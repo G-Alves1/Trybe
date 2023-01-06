294a268abb01e27a29609baa5398cedf86c34683
G-Alves1 <2kgabrielalves@gmail.com>
2023-01-06 07:07:21 -0300

    feat(): Block 09 - Day 01

---
+++ 01_Fundamentos-do-Desenvolvimento-Web/Bloco09_JavaScript-e-Testes-Assincronos/Dia01_JavaScript-Assincrono-e-Callbacks/script.js
@@ -0,0 +1,64 @@
+/*******1*******/
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn)
+};
+
+console.log(getUser(userFullName));
+console.log(getUser(userNationality));
+
+/*******2*******/
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    console.log(callback(user));
+  }, delay());
+};
+
+getUser(userFullName);
+getUser(userNationality);
+
+/*******3*******/
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, pErrorMessage) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      pErrorMessage(errorMessage);
+    }
+  }, delay());
+};
+
+getCountry(countryName, printErrorMessage);
+getCountry(countryCurrency, printErrorMessage);
