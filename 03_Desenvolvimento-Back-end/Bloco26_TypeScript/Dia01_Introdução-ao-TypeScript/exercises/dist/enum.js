fea76065609c7e608671c8c69281ea5029c062a8
G-Alves1 <2kgabrielalves@gmail.com>
2023-01-06 20:31:36 -0300

    feat(): Block 26 - Day 01

---
+++ 03_Desenvolvimento-Back-end/Bloco26_TypeScript/Dia01_Introdução-ao-TypeScript/exercises/dist/enum.js
@@ -0,0 +1,21 @@
+"use strict";
+var DaysOfWeek;
+(function (DaysOfWeek) {
+  DaysOfWeek[(DaysOfWeek["Sunday"] = 1)] = "Sunday";
+  DaysOfWeek[(DaysOfWeek["Monday"] = 2)] = "Monday";
+  DaysOfWeek[(DaysOfWeek["Tuesday"] = 3)] = "Tuesday";
+  DaysOfWeek[(DaysOfWeek["Wednesday"] = 4)] = "Wednesday";
+  DaysOfWeek[(DaysOfWeek["Thursday"] = 5)] = "Thursday";
+  DaysOfWeek[(DaysOfWeek["Friday"] = 6)] = "Friday";
+  DaysOfWeek[(DaysOfWeek["Saturday"] = 7)] = "Saturday";
+})(DaysOfWeek || (DaysOfWeek = {}));
+var RainbowColors;
+(function (RainbowColors) {
+  RainbowColors["Red"] = "Vermelho";
+  RainbowColors["Orange"] = "Laranja";
+  RainbowColors["Yellow"] = "Amarelo";
+  RainbowColors["Green"] = "Verde";
+  RainbowColors["Blue"] = "Azul";
+  RainbowColors["Indigo"] = "Anil";
+  RainbowColors["Violet"] = "Violeta";
+})(RainbowColors || (RainbowColors = {}));
---
+++ 03_Desenvolvimento-Back-end/Bloco26_TypeScript/Dia01_Introdução-ao-TypeScript/exercises/enum.ts
@@ -0,0 +1,19 @@
+enum DaysOfWeek {
+  Sunday = 1,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+enum RainbowColors {
+  Red = "Vermelho",
+  Orange = "Laranja",
+  Yellow = "Amarelo",
+  Green = "Verde",
+  Blue = "Azul",
+  Indigo = "Anil",
+  Violet = "Violeta",
+}
